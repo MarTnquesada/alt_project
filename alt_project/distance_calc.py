5bede39bd405432de60bda2c3e5d7ae4c1f1eba7
QUESADA ZARAGOZA <marqueza@EVIRL-005-OK.dsic.cloud>
2019-09-27 14:01:03 +0200

    created distance calculations file

--- alt_project/distance_calc.py
+++ alt_project/distance_calc.py
@@ -1,6 +1,4 @@
 
-
-
 def hamming(chain1, chain2):
     if len(list(chain1)) != len(list(chain2)):
         return -1
@@ -13,8 +11,22 @@ def hamming(chain1, chain2):
 
 
 def levenshtein(chain1, chain2, threshold):
-    distance = 0
+    return levenshtein_base(chain1,chain2, threshold, 0, 0)
 
+
+def levenshtein_base(chain1, chain2, threshold, pos1, pos2):
+    if pos1 == 0:
+        if pos2 == 0:
+            return 0
+        else:
+            return levenshtein_base(chain1, chain2, pos1, pos2-1) + 1
+    elif pos2 == 0:
+        return levenshtein_base(chain1, chain2, pos1-1, pos2) + 1
+    else:
+        return min(levenshtein_base(chain1, chain2, pos1-1, pos2-1),
+                   levenshtein_base(chain1, chain2, pos1, pos2-1),
+                   levenshtein_base(chain1, chain2, pos1-1, pos2)) 
+                   + 1
 
 
 def damerau_levenshtein(chain1, chain2, threshold):
--- tests/test_distance_calc.py
+++ tests/test_distance_calc.py
@@ -4,6 +4,13 @@ from alt_project.distance_calc import levenshtein, damerau_levenshtein
 def class TestDistanceCalc():
 
     def test_levenshtein(self):
+        chain1 = "AACBDCE"
+        chain2 = "ACBBBCF"
+        correct_distance = 4
+        obtained_distance = hamming(chain1, chain2)
+        assert correct_distance == obtained_distance
+
+    def test_levenshtein(self):
         chain1 = "AACBDC"
         chain2 = "ACBBBC"
         correct_distance = 3
